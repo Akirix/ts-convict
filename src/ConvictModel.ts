c89a312b170c179272262b1c154b1f8eeb5f4f8c
Kelly Ferrone <4399427+kferrone@users.noreply.github.com>
2019-10-02 15:46:39 -0600

    missed some docs

--- src/ConvictModel.ts
+++ src/ConvictModel.ts
@@ -112,6 +112,9 @@ export class ConvictModel {
         return getMetaSchemaStorage().parseSchema(alias);
     }
 
+    /**
+     * Completely removes all meta items from the global repo
+     */
     public clear() {
         return getMetaSchemaStorage().clearRepo();
     }
